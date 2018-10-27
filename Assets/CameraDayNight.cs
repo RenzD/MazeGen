098c3244da0b08cb8a5419b4647800e17532b95e
Morris Arroyo <arroyomorris28@gmail.com>
2018-10-27 13:52:04 -0700

    changed skybox day night

---
+++ Assets/CameraDayNight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDayNight : MonoBehaviour
+{
+    Camera cam;
+    Color skyBlue;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        skyBlue = cam.backgroundColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!cam.backgroundColor.Equals(Color.black))
+            {
+                cam.backgroundColor = Color.black;
+            }
+            else
+            {
+                cam.backgroundColor = skyBlue;
+            }
+        }
+    }
+}
